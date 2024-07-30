8892d119fa74886b444770d5d04d344289ca58d2
KGr1sha <krylov.grigory@gmail.com>
2024-07-30 13:11:56 +0300

    add new tileset

--- scripts/player/movement.gd
+++ scripts/player/movement.gd
@@ -1,36 +1,38 @@
 extends CharacterBody2D
 
-@export var MAX_SPEED = 300
+@export var MAX_SPEED : float = 300
 @export var FRICTION: float = 1200
-@export var ACCELERATION = 1500
-
+@export var ACCELERATION : float = 1500
 
 @onready var axis = Vector2.ZERO
 
 
-func _process(delta):
+func _process(_delta):
 	axis = get_input_axis()
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	move()
 
 
-func get_input_axis():
-	# axis.x = int(Input.is_action_pressed('move_right')) - int(Input.is_action_pressed('move_left'))
-	# axis.y = int(Input.is_action_pressed('move_down')) - int(Input.is_action_pressed('move_up'))
-	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+func get_input_axis() -> Vector2:
+	return Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
 
 func move():
 	if axis == Vector2.ZERO:
-		apply_fiction(FRICTION)
+		apply_friction(FRICTION)
 	elif axis != Vector2.ZERO:
 		apply_movement(axis * ACCELERATION)
 
 	move_and_slide()
 
 
-func apply_fiction(amount):
+func apply_friction(amount):
 	if velocity.length() > amount:
 		velocity -= velocity.normalized() * amount
 	else:
