df0aa59b151dcac8cfdefb755736eef82782177d
KGr1sha <krylov.grigory@gmail.com>
2024-08-01 12:55:35 +0300

    figured how to place rooms

--- scripts/procedural_generation/generator.gd
+++ scripts/procedural_generation/generator.gd
@@ -3,24 +3,42 @@ extends Node2D
 @export var unit_size : int = 16
 @export var grid_size_x : int = 30
 @export var grid_size_y : int = 30
-var grid : Array[Array]
-var room_scene = preload("res://scenes/room.tscn")
+@export var roomcnt : int = 5
+var is_room : Array[Array] # stores info about placed rooms
+var packed_room : PackedScene
 
+	
 func _ready() -> void:
-	for i in range(grid_size_x):
-		for j in range(grid_size_y):
-			grid[i][j] = false
-	
-	
-	
+	packed_room = load("res://scenes/room.tscn")
+	generate_rooms()
+
+
+func generate_rooms() -> void:
+	var x : int
+	var y : int
+	for i in range(roomcnt):
+		x = randi() % grid_size_x
+		y = randi() % grid_size_y
+		# todo: check for room overlap
+		place_room(Vector2i(x, y))
 
 
 func place_room(pos : Vector2i) -> bool:
-	var room : Room = room_scene.instantiate()
-	room.position = pos * unit_size
-	for i in range(pos.y - room.size.y / 2, pos.y + room.size.y / 2):
-		for j in range(pos.x - room.size.x / 2, pos.x + room.size.x / 2):
-			grid[i][j] = true
+	var room_instance : Room = packed_room.instantiate()
+	add_child(room_instance)
+	room_instance.position = pos * unit_size
+	register_room(pos, room_instance.size)
 	
 	return true
+
+
+func register_room(pos : Vector2i, size : Vector2i) -> void:
+	var left = clampi(pos.x - size.x / 2, 0, grid_size_x)
+	var right = clampi(pos.x + size.x / 2, 0, grid_size_x)
+	var bottom = clampi(pos.y - size.y / 2, 0, grid_size_y)
+	var up = clampi(pos.y + size.y / 2, 0, grid_size_y)
+
+	for i in range(up, bottom):
+		for j in range(left, right):
+			is_room[i][j] = true
 
